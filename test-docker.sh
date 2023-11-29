486a5bc65747254bda2b140ea4b8fd5fe3649fb9
Baxter Rogers <baxter.rogers@vumc.org>
2023-11-29 12:57:24 -0600

    tester

--- test-docker.sh
+++ test-docker.sh
@@ -2,7 +2,6 @@
 
 docker run --read-only \
     --mount type=bind,src=$(pwd -P)/binds/tmp,dst=/tmp \
-    --mount type=bind,src=$(pwd -P)/binds/root,dst=/root \
     baxterprogers/matlabtest:v0.0.0
 
 exit 0
